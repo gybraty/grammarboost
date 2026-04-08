3a138b4cd75fd3bd25f88548f708b697c07da093
gybraty <220001@astanait.edu.kz>
2026-04-09 01:21:47 +0500

    project init

---
+++ client/pages/index.js
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import { Geist, Geist_Mono } from "next/font/google";
+import { Button } from "@/components/ui/button";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function Home() {
+  return (
+    <div
+      className={`${geistSans.className} ${geistMono.className} flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black`}
+    >
+      <Button variant="outline" size="lg">
+        Hello World
+      </Button>
+    </div>
+  );
+}
