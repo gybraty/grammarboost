9f899e873f8ed6937fe0f2bdb699035f6ab2ed18
gybraty <220001@astanait.edu.kz>
2026-05-02 11:56:33 +0500

    fix regex wildcard

--- server/app.js
+++ server/app.js
@@ -68,7 +68,7 @@ app.use('/api', notFound);
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', (req, res) => {
+app.get('{*path}', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
